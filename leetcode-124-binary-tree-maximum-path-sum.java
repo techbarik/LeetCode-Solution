e2e97c484f1ab1b6c846a7c9bbdb60a832568296
Md. A. Barik <68262744+techbarik@users.noreply.github.com>
2020-07-30 12:45:40 +0600

    Create leetcode-124-binary-tree-maximum-path-sum.java

---
+++ leetcode-124-binary-tree-maximum-path-sum.java
@@ -0,0 +1,24 @@
+/*
+  Contributed By: Md. A. Barik
+  Video Explanation: https://youtu.be/XLPuk-V_DcY
+*/
+
+public class Solution {
+    int max = Integer.MIN_VALUE;
+    
+    public int maxPathSum(TreeNode root) {
+        helper(root);
+        return max;
+    }
+    
+    int helper(TreeNode root) {
+        if (root == null) return 0;
+        
+        int left = Math.max(helper(root.left), 0);
+        int right = Math.max(helper(root.right), 0);
+        
+        max = Math.max(max, root.val + left + right);
+        
+        return root.val + Math.max(left, right);
+    }
+}

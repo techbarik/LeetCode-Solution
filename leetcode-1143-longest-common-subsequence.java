b5a4c871747facb75656ef9f38f7ca634a7a2108
Md. A. Barik <68262744+techbarik@users.noreply.github.com>
2020-08-05 13:56:54 +0600

    Create leetcode-1143-longest-common-subsequence.java

---
+++ leetcode-1143-longest-common-subsequence.java
@@ -0,0 +1,28 @@
+/*
+  Contributed by: Md. A. Barik
+  Video Explanation: https://youtu.be/hckjpI5JIl0
+*/
+
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        char[] str1 = text1.toCharArray();
+        char[] str2 = text2.toCharArray();
+        int temp[][] = new int[str1.length + 1][str2.length + 1];
+        int max = 0;
+        for(int i=1; i < temp.length; i++){
+            for(int j=1; j < temp[i].length; j++){
+                if(str1[i-1] == str2[j-1]) {
+                    temp[i][j] = temp[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    temp[i][j] = Math.max(temp[i][j-1],temp[i-1][j]);
+                }
+                if(temp[i][j] > max) {
+                    max = temp[i][j];
+                }
+            }
+        }
+        return max;
+    }
+}

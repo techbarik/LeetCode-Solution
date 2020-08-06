87af05b6fa5b5e5dc53b58509a5a109e5374df43
Md. A. Barik <68262744+techbarik@users.noreply.github.com>
2020-08-06 23:23:26 +0600

    Create leetcode-49-group-anagrams.java

---
+++ leetcode-49-group-anagrams.java
@@ -0,0 +1,19 @@
+/*
+  Contributed by: Md. A. Barik
+  Video Explanation: https://youtu.be/M0HwPE0u7lY
+*/
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            System.out.println(key);
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
+}

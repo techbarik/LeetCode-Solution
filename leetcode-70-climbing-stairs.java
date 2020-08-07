0d82fa555508210c21ac5586a3f838d8009fb5fa
Md. A. Barik <68262744+techbarik@users.noreply.github.com>
2020-08-07 17:33:05 +0600

    Create leetcode-70-climbing-stairs.java

---
+++ leetcode-70-climbing-stairs.java
@@ -0,0 +1,18 @@
+/*
+  Contributed by: Md. A. Barik
+  Video Explnation: https://youtu.be/LJYOw-G-xfk
+*/
+
+public class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) return 1;
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+}

ea1080650a62015c2055f4be91235d57e397c6a9
Md. A. Barik <68262744+techbarik@users.noreply.github.com>
2020-11-06 07:43:47 +0600

    Update leetcode-24-swap-nodes-in-pairs-recursive-solution.java

--- leetcode-24-swap-nodes-in-pairs-recursive-solution.java
+++ leetcode-24-swap-nodes-in-pairs-recursive-solution.java
@@ -1,15 +1,16 @@
 
 public class Solution {
     public ListNode swapPairs(ListNode head) {
-		if (head == null || head.next == null) 
-			return head;
-		ListNode p1 = head, p2 = head.next, p3 = p2.next;
-		p1.next = p3;
-		p2.next = p1;
-		
-		if (p3 != null) {
-			p1.next = swapPairs(p3);
-		}
-		
-		return p2;
+	if (head == null || head.next == null) 
+		return head;
+	ListNode p1 = head, p2 = head.next, p3 = p2.next;
+	p1.next = p3;
+	p2.next = p1;
+
+	if (p3 != null) {
+		p1.next = swapPairs(p3);
 	}
+
+	return p2;
+    }
+}
